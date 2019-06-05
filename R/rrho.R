873135c5eb57da649c678d4625656e8c9e2a2679
Anthony Piron <anthony.piron@skynet.be>
2019-06-05 18:08:17 +0200

    Fix intersection

--- R/rrho.R
+++ R/rrho.R
@@ -1,4 +1,6 @@
 
+library(reshape2)
+library(ggplot2)
 
 defaultStepSize <- function (list1, list2)
 {
@@ -8,25 +10,27 @@ defaultStepSize <- function (list1, list2)
 numericListOverlap <- function(sample1, sample2, stepsize, alternative)
 {
     n <- length(sample1)
-    ## print(sample1)
-    
     overlap <- function(a, b)
     {
-        count <- as.integer(sum(as.numeric(names(sample1)[1:a] %in% sample2[1:b])))
+        count <- as.integer(sum(as.numeric(sample1[1:a] %in% sample2[1:b])))
+        the.mean <- a*b/n
         
         switch(alternative,
                enrichment={
-                   pval<- phyper(q=count-1,
-                                 m=a, n=n-a+1, k=b,
-                                 lower.tail=FALSE)
+                   pval <- phyper(q=count-1,
+                                  m=a, n=n-a+1, k=b,
+                                  lower.tail=FALSE)
+                   fdr <- the.mean / count
                    signs <- 1L
                },
                two.sided={
-                   the.mean <- a*b/n
                    signs <- sign(count - the.mean)
                    symmetric <- 2*the.mean - count
                    lower <- min(count, symmetric)
                    upper <- max(count, symmetric)
+
+                   ## TODO: is this right? Think so.
+                   fdr <- the.mean / upper
                    
                    pval <- phyper(q=lower,
                                   m=a, n=n-a+1, k=b,
@@ -36,9 +40,10 @@ numericListOverlap <- function(sample1, sample2, stepsize, alternative)
                               lower.tail=FALSE)
                })
         
-        return(c(counts=count,
-                 pval=as.numeric(pval),
-                 signs=as.integer(signs)))    
+        return( c(counts=count,
+                  pval=as.numeric(pval),
+                  signs=as.integer(signs),
+                  fdr=as.numeric(fdr)) )    
     }
   
     logging(paste0("n = ", n, " ; stepsize = ", stepsize),
@@ -52,17 +57,18 @@ numericListOverlap <- function(sample1, sample2, stepsize, alternative)
     matrix.counts <- matrix(overlaps['counts',], ncol=nrows)
     matrix.pvals <- matrix(overlaps['pval',], ncol=nrows)
     matrix.signs <- matrix(overlaps['signs',], ncol=nrows)  
+    matrix.fdr <- matrix(overlaps['fdr',], ncol=nrows)  
   
     return(list(counts=matrix.counts,
                 pval=matrix.pvals,
-                signs=matrix.signs))  
+                signs=matrix.signs,
+                fdr=matrix.fdr))  
 }
 
 #' @export RRHO
 RRHO <- function(list1, list2, 
                  stepsize=NULL, 
-                 alternative="two.sided",
-                 log10.ind=FALSE)
+                 alternative="two.sided")
 {
     checkAction((alternative=='two.sided' || alternative=='enrichment'),
                 .message="Alternative should be 'two.sided' or 'enrichment'.",
@@ -76,7 +82,7 @@ RRHO <- function(list1, list2,
     list1 <- list1[inter]
     list2 <- list2[inter]
         
-    ## Order lists along list2
+    ## Order lists
     list1  <- list1[order(list1[inter],decreasing=TRUE)]
     list2  <- list2[order(list2[inter],decreasing=TRUE)]
     nlist1 <- length(list1)
@@ -89,5 +95,52 @@ RRHO <- function(list1, list2,
     hypermat$padj <- matrix(p.adjust(hypermat$pval, method="BY"),
                             nrow=nrow(hypermat$pval))
 
-    return(hypermat)
+    return(structure(hypermat,
+                     class="rrho"))
 }
+
+#' @export
+plot <- function (rrho, ...)
+{
+    UseMethod("plot", rrho)
+}
+
+#' @export
+plot.rrho <- function (rrho, min.pval=1e-12,
+                       colors=c('darkblue', 'darkgreen',
+                                'darkorange', 'darkred'))
+{
+    max.log <- -log(min.pval)
+    no.zero <- apply(rrho$padj, 1:2,
+                     function (p) min(p + min.pval, 1) )
+    signed.log.pval <- -log(no.zero) * rrho$signs 
+
+    b <- c(-max.log, 0, max.log)
+
+    ggplot(data = melt(signed.log.pval),
+           aes(x=Var1, y=Var2, fill=value)) + 
+        geom_tile() +
+        scale_fill_gradientn(colors = colors, breaks = b,
+                             labels = format(b),
+                             limits=b[c(1,length(colors))],
+                             name="-log p.val") + 
+        theme(axis.title.x=element_blank(),
+              axis.text.x=element_blank(),
+              axis.ticks.x=element_blank(),
+              axis.title.y=element_blank(),
+              axis.text.y=element_blank(),
+              axis.ticks.y=element_blank(),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.background = element_blank(),)
+}
+
+
+
+## n <- 100
+## list1 <- 1:n
+## names(list1) <- 1:n
+## list2 <- 1:n
+## names(list2) <- 1:n
+## rrho <- RRHO(list1,list2)
+## plot(rrho)
