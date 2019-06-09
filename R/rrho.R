9060e80ea8c8e0012b54a5e6ff454c9c20f971e7
Anthony Piron <anthony.piron@skynet.be>
2019-06-09 21:44:31 +0200

    Add recursive best p-val

--- R/rrho.R
+++ R/rrho.R
@@ -110,17 +110,50 @@ getDown.index  <- function(l)
         length(l)+1
 }
 
+intersect.clean <- function(mat, decreasing=TRUE)
+{
+    op <- if (decreasing) `<` else `>`
+    
+    rec <- function(mat)
+    {
+        if (nrow(mat) > 0)
+        {
+            higher <- intersect.clean(mat[op(mat[1,2], mat[,2]),, drop=FALSE])
+            return(rbind(mat[1,], higher))
+        }
+        return(mat)
+    }
+    mat <- mat[order(mat[,1], mat[,2], decreasing=decreasing),,
+                 drop=FALSE]
+    
+    return(rec(mat))
+}
+
+getUPUP.ij <-  function(rrho)
+{
+    if (is.null(rrho$best$upup))
+    {
+        ## -1 because half-brained started the R indexes at 1
+        ## -1 because getDown.index gives the first negative index
+        x.ind <- (getDown.index(rrho$list1) - 2) %/% rrho$stepsize + 1
+        y.ind <- (getDown.index(rrho$list2) - 2) %/% rrho$stepsize + 1
+        logging(paste0("x = ", x.ind, " ; y = ", y.ind),
+                .module="RRHO")
+        ## start from 2 because phyper does not make any sense for 1
+        UP.mat <- ifelse(rrho$sign[2:x.ind,2:y.ind,drop=FALSE] < 0, 1,
+                         rrho$padj[2:x.ind,2:y.ind,drop=FALSE])
+        UP.fdr <- rrho$fdr[2:x.ind,2:y.ind,drop=FALSE]
+        upup <- which(UP.mat == min(UP.mat), arr.ind=TRUE) + 1
+        upup <- intersect.clean(upup)
+        rrho$best$upup <- upup
+    }
+    
+    return(rrho$best$upup)
+}
+
 getUPUP <- function(rrho, fdr=0.3)
 {
-    x.ind <- (getDown.index(rrho$list1) - 1) %/% rrho$stepsize + 1
-    y.ind <- (getDown.index(rrho$list2) - 1) %/% rrho$stepsize + 1
-    logging(paste0("x = ", x.ind, " ; y = ", y.ind),
-            .module="RRHO")
-    ## start from 2 because phyper does not make any sense for 1
-    UP.mat <- ifelse(rrho$sign[2:x.ind,2:y.ind,drop=FALSE] < 0, 1,
-                     rrho$padj[2:x.ind,2:y.ind,drop=FALSE])
-    UP.fdr <- rrho$fdr[2:x.ind,2:y.ind,drop=FALSE]
-    min.ind  <- which(UP.mat == min(UP.mat), arr.ind=TRUE)
+    min.ind  <- getUPUP.ij(rrho)
     logging(paste0("min.ind = ", min.ind[1,1], ", ", min.ind[1,2] ),
             .module="RRHO")
     logging(paste0("fdr = ", UP.fdr[min.ind[1,1], min.ind[1,2]] ),
@@ -129,21 +162,34 @@ getUPUP <- function(rrho, fdr=0.3)
               names(rrho$list2)[1:(min.ind[1,2] * rrho$stepsize)])
 }
 
+getDOWNDOWN.ij <-  function(rrho)
+{
+    if (is.null(rrho$best$downdown))
+    {
+        ## TODO: remove last line, not needed if len(list)+1
+        ## is not divisible by stepsize
+        x.len <- nrow(rrho$padj) - 1
+        ## -1 because half-brained started the R indexes at 1
+        x.ind <- (getDown.index(rrho$list1)-1) %/% rrho$stepsize + 1
+        y.len <- ncol(rrho$padj) - 1
+        y.ind <- (getDown.index(rrho$list2)-1) %/% rrho$stepsize + 1
+        logging(paste0("x = ", x.ind, " ; y = ", y.ind),
+                .module="RRHO")
+        ## 
+        DOWN.mat <- ifelse(rrho$sign[x.ind:x.len,y.ind:y.len,drop=FALSE] < 0, 1,
+                           rrho$padj[x.ind:x.len,y.ind:y.len,drop=FALSE])
+        DOWN.fdr <- rrho$fdr[x.ind:x.len,y.ind:y.len,drop=FALSE]
+        downdown <- t(t(which(DOWN.mat == min(DOWN.mat), arr.ind=TRUE)) +
+                                c(x.ind-1,y.ind-1))
+        downdown <- intersect.clean(downdown, decreasing=FALSE)
+        rrho$best$downdown <- downdown
+    }
+    return(rrho$best$downdown)
+}
+
 getDOWNDOWN <- function(rrho, fdr=0.3)
 {
-    ## remove last line
-    x.len <- nrow(rrho$padj) - 1
-    ## TODO: start above midpoint in logFC
-    x.ind <- getDown.index(rrho$list1) %/% rrho$stepsize + 1
-    y.len <- ncol(rrho$padj) - 1
-    y.ind <- getDown.index(rrho$list2) %/% rrho$stepsize + 1
-    logging(paste0("x = ", x.ind, " ; y = ", y.ind),
-            .module="RRHO")
-    ## 
-    DOWN.mat <- ifelse(rrho$sign[x.ind:x.len,y.ind:y.len,drop=FALSE] < 0, 1,
-                     rrho$padj[x.ind:x.len,y.ind:y.len,drop=FALSE])
-    DOWN.fdr <- rrho$fdr[x.ind:x.len,y.ind:y.len,drop=FALSE]
-    min.ind  <- which(DOWN.mat == min(DOWN.mat), arr.ind=TRUE)
+    min.ind <- getDOWNDOWN.ij(rrho)
     logging(paste0("fdr = ", DOWN.fdr[min.ind[1,1], min.ind[1,2]] ),
             .module="RRHO")
     min.ind <- t(t(min.ind) + c(x.ind, y.ind))
@@ -197,46 +243,64 @@ plot.rrho <- function (rrho,
                               gp=grid::gpar(fontsize=13, fontface="bold"))
     vperc <- ncol(rrho$pval) / 10
     hperc <- nrow(rrho$pval) / 10
-    gg <- ggplot2::ggplot(data = melt(signed.log.pval),
-                 aes(x=Var1, y=Var2, fill=value)) + 
-        ggplot2::geom_tile() +
+    upup <- as.data.frame(getUPUP.ij(rrho))
+    upup$padj <- apply(upup,1,function (x) rrho$padj[x[1],x[2]])
+    downdown <-  as.data.frame(getDOWNDOWN.ij(rrho))
+    downdown$padj <- apply(downdown,1,function (x) rrho$padj[x[1],x[2]])
+    gg <- ggplot2::ggplot() + 
+        ggplot2::geom_tile(data = melt(signed.log.pval),
+                           aes(x=Var1, y=Var2, fill=value)) +
         ggplot2::scale_fill_gradientn(colors = colors, breaks = b,
-                             labels = format(b),
-                             limits=b[c(1,length(colors))],
-                             name="-log p.val") + 
+                                      labels = format(b),
+                                      limits=b[c(1,length(colors))],
+                                      name="-log p.val") + 
         ggplot2::theme(##axis.title.x=element_blank(),
-            axis.text.x=ggplot2::element_blank(),
-            axis.ticks.x=ggplot2::element_blank(),
-            ##axis.title.y=element_blank(),
-            axis.text.y=ggplot2::element_blank(),
-            axis.ticks.y=ggplot2::element_blank(),
-            panel.grid.major = ggplot2::element_blank(),
-            panel.grid.minor = ggplot2::element_blank(),
-            panel.background = ggplot2::element_blank()) +
+                     axis.text.x=ggplot2::element_blank(),
+                     axis.ticks.x=ggplot2::element_blank(),
+                     ##axis.title.y=element_blank(),
+                     axis.text.y=ggplot2::element_blank(),
+                     axis.ticks.y=ggplot2::element_blank(),
+                     panel.grid.major = ggplot2::element_blank(),
+                     panel.grid.minor = ggplot2::element_blank(),
+                     panel.background = ggplot2::element_blank()) +
         ggplot2::xlab(labels[1]) +  ggplot2::ylab(labels[2]) +
         ggplot2::annotation_custom(text_up,
-                          xmin=hperc,xmax=hperc,ymin=-1.2,ymax=-1.2) +
+                                   xmin=hperc,xmax=hperc,ymin=-1.2,ymax=-1.2) +
         ggplot2::annotation_custom(text_down,
-                          xmin=nrow(rrho$pval)+hperc,
-                          xmax=nrow(rrho$pval)-3*hperc,
-                          ymin=-1.2,ymax=-1.2) +
+                                   xmin=nrow(rrho$pval)+hperc,
+                                   xmax=nrow(rrho$pval)-3*hperc,
+                                   ymin=-1.2,ymax=-1.2) +
         ggplot2::annotation_custom(text_up_rot,
-                          xmin=-1.8,xmax=-1.8,
-                          ymin=vperc,ymax=vperc) +
+                                   xmin=-1.8,xmax=-1.8,
+                                   ymin=vperc,ymax=vperc) +
         ggplot2::annotation_custom(text_down_rot,
-                          ymin=ncol(rrho$pval)+vperc,
-                          ymax=ncol(rrho$pval)-3*vperc,
-                          xmin=-1.8,xmax=-1.8)
+                                   ymin=ncol(rrho$pval)+vperc,
+                                   ymax=ncol(rrho$pval)-3*vperc,
+                                   xmin=-1.8,xmax=-1.8) +
+        ggplot2::geom_point(data=upup,
+                            aes(x=row, y=col)) +
+        ggplot2::geom_text(data=upup,
+                           aes(x=row, y=col,
+                               label=formatC(padj,
+                                             format = "e", digits = 2)),
+                           hjust="inward", vjust="inward") +
+        ggplot2::geom_point(data=downdown,
+                            aes(x=row, y=col)) +
+        ggplot2::geom_text(data=downdown,
+                           aes(x=row, y=col,
+                               label=formatC(padj,
+                                             format = "e", digits = 2)),
+                           hjust="inward", vjust="inward")
     x.ind <- which(rrho$list1 < 0)
     if ( length(x.ind) > 0 )
         gg  <- gg +
-            ggplot2::geom_vline(aes(xintercept = x.ind[1] / rrho$stepsize), 
-                       linetype = "dotted", colour = "gray10",size = 0.5)
+            ggplot2::geom_vline(aes(xintercept = (x.ind[1]-1) / rrho$stepsize + 1), 
+                       linetype = "dotted", colour = "gray10",size = 1)
     y.ind <- which(rrho$list2 < 0)
     if ( length(y.ind) > 0 )
         gg  <- gg +
-            ggplot2::geom_hline(aes(yintercept = y.ind[1] / rrho$stepsize), 
-                       linetype = "dotted", colour = "gray10",size = 0.5)
+            ggplot2::geom_hline(aes(yintercept = (y.ind[1]-1) / rrho$stepsize + 1), 
+                       linetype = "dotted", colour = "gray10",size = 1)
 
     return(gg)
 }
